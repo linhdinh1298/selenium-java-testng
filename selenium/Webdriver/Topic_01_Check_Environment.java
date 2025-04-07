9642ace126b404c9e0da10cab01b66e019f51e2e
linhdinh <linhdtm11111@gmail.com>
2025-04-08 06:29:07 +0700

    add new class

--- selenium/Webdriver/Topic_01_Check_Environment.java
+++ selenium/Webdriver/Topic_01_Check_Environment.java
@@ -12,6 +12,7 @@ public class Topic_01_Check_Environment {
     public void TC_01_Run_On_Firefox() {
         driver = new FirefoxDriver();
         driver.get("https://www.facebook.com/");
+        // add new line
         driver.quit();
     }
 
