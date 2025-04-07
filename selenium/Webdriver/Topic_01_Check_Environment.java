ec79c41290f6775c7d93d3f3db4a7aaef794217b
linhdinh1298 <linhdtm11111@gmail.com>
2025-04-08 06:53:02 +0700

    update topic

--- selenium/Webdriver/Topic_01_Check_Environment.java
+++ selenium/Webdriver/Topic_01_Check_Environment.java
@@ -12,7 +12,6 @@ public class Topic_01_Check_Environment {
     public void TC_01_Run_On_Firefox() {
         driver = new FirefoxDriver();
         driver.get("https://www.facebook.com/");
-        // add new line
         driver.quit();
     }
 
